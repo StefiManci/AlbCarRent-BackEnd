7789177980d11e88df4876a36f3412147a64ac95
Stefi <stefi.manci1@gmail.com>
2025-12-18 10:26:24 +0100

    Database Creation

---
+++ AlbCarRent/Datalayer/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlbCarRent.Datalayer
+{
+    public class AppUser : IdentityUser
+    {
+        public string FullName { get; set; }
+    }
+
+    public class ApplicationDbContext : IdentityDbContext<AppUser, IdentityRole, string> { 
+    
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options){}
+
+    }
+
+}
--- AlbCarRent/Program.cs
+++ AlbCarRent/Program.cs
@@ -1,3 +1,8 @@
+using AlbCarRent.Datalayer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
@@ -5,9 +10,24 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+//Configure EF COre with SQL Server
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+//Configure Identity
+
+builder.Services.AddIdentity<AppUser, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 6;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
